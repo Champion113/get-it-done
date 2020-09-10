7c1157440fab065b1c074fe086d57aa4ae871301
Champion113 <sbalbright19@gmail.com>
2020-09-10 06:58:54 -0400

    added fetch

--- assets/homepage.js
+++ assets/homepage.js
@@ -4,8 +4,38 @@ var getUserRepos = function(user) {
   
     // make a request to the url
     fetch(apiUrl).then(function(response) {
-      response.json().then(function(data) {
-        console.log(data);
+        response.json().then(function(data) {
+            displayRepos(data, user);
+          });
       });
-    });
+    };
+  ;
+//store reference to form element
+  var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+//function executed upon a form submission browser event
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    console.log(event);
+    // get value from input element
+var username = nameInputEl.value.trim();
+
+if (username) {
+  getUserRepos(username);
+  nameInputEl.value = "";
+} else {
+  alert("Please enter a GitHub username");
+}
   };
+
+  var displayRepos = function(repos, searchTerm) {
+    console.log(repos);
+    console.log(searchTerm);
+  };
+
+
+
+
+
+
+  userFormEl.addEventListener("submit", formSubmitHandler);  
